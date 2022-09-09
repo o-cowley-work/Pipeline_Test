3b9ca392c7fa0f035563d291573afa76c9c10af8
oliverIwork <oliver.cowley@intel.com>
2022-09-09 10:22:36 -0700

    Pipeline functions

--- test_pipeline.groovy
+++ test_pipeline.groovy
@@ -1,58 +1,25 @@
-pipeline {
-	agent any
-	stages {
-	    
-		stage('Build') {
-			steps {
-				echo 'Doing a second thing'
-				bat 'Build.bat'
-			}
-		}
+def build_fn() {
+	echo 'Doing a second thing'
+	bat 'Build.bat'
+}
+
+def unit_fn(){
+	echo 'JUnit Successfully'
+	bat 'Unit.bat'			
+}
 		
-		stage('JUnit') {
-			steps {
-				echo 'JUnit Successfully'
-				bat 'Unit.bat'
-			}
-		}
+def qual_fn() {
+	echo 'Qual Successfully'
+	bat 'Quality.bat'
+}
+
 		
-		stage('Quality') {
-			steps {
-				echo 'Qual Successfully'
-				bat 'Quality.bat'
-			}
-		}
-		
-		stage('Deploy') {
-			steps {
+def deploy_fn(){
 				echo 'Pass'
 				bat 'Deploy.bat'
-			}
-		}
-	}
-	
-	post {
-		always {
-			echo 'Run Always'
-		}
-		
-		success {
-			echo 'Run on pass'
-		}
-		
-		failure {
-			echo 'Run on failure'
-		}
-		
-		unstable {
-			echo 'Run on unstable'
-		}
-		
-		changed {
-			echo 'Run if pipeline changed status'
-		}
-	}
 }
+	
+
 		
 		
 		
