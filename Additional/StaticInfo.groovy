40fd24b582f539b08d0028e93929e1d4baa16252
Oliver Cowley <oliver.cowley@intel.com>
2022-09-23 13:55:14 -0700

    Checking the need for a field designation on imported scripts, as well as checking if child import scripts can access the parent's imported stuff

--- Additional/StaticInfo.groovy
+++ Additional/StaticInfo.groovy
@@ -1,7 +1,7 @@
 
 import groovy.transform.Field
 
-public static final TEST_ME = "TEST IF I AM VISIBLE"
+@Field public static final TEST_ME = "TEST IF I AM VISIBLE"
 
 @Field public static final TEST_ME2 = "I HAVE A FIELD DESIGATION"
 
