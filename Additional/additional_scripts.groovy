89c4ebeefae94c47939da6dc7b99672785c33ba4
Oliver Cowley <oliver.cowley@intel.com>
2022-09-26 14:53:43 -0700

    :(

--- Additional/additional_scripts.groovy
+++ Additional/additional_scripts.groovy
@@ -10,4 +10,9 @@ def doStuff(String arg1) {
         
 }
 
+def runProps2(String add_name="${env.PROJECT_NAMES}") {
+    properties([parameters([string(name: 'PROJECT_NAMES', defaultValue: add_name, description: "Project name(s), available projects: ${env.PROJECT_NAMES} or ALL"),
+                                string(name: 'ANOTHER_THING', defaultValue: env.ANOTHER_THING, description: "SOME OTHER THING: ${env.ANOTHER_THING} or ALL")])])
+}
+
 return this;
